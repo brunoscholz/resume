85d5edb8c522b733c19ab6862413102a6b02b5d3
Bruno Ribeiro Scholz <bruno.scholz@possible.com>
2022-06-02 16:44:25 -0300

    initial content

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Profile from "./components/Profile";
+import Experience from "./components/Experience";
+
+function App() {
+  return (
+    <div className="App">
+      <Profile />
+      <hr />
+      <Experience />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,39 @@
+import './Profile.css'
+import image from '../headshot.png'
+
+import { AiFillTwitterCircle, AiFillGithub, AiFillLinkedin } from 'react-icons/ai'
+
+const Profile = () => {
+  return (
+    <div className='section'>
+      <div className='bio'>
+        <p className='no-margin small-text'>Hello</p>
+        <p className='no-margin name-text'>
+          I'm <span className='highlight-text'>Tim Ruscica</span>
+        </p>
+        <p className='no-margin small-text'>
+          youtuber <span className='highlight-text'>and</span> software engineer
+        </p>
+        <div className='icons'>
+          <a href='https://twitter.com/brunoskolz'>
+            <AiFillTwitterCircle color='orange' size={30} />
+          </a>
+          {/* <a href='https://youtube.com/techwithtim'>
+            <AiFillYoutube color='orange' size={30} />
+          </a> */}
+          <a href='https://github.com/brunoscholz'>
+            <AiFillGithub color='orange' size={30} />
+          </a>
+          <a href='https://linkedin.com/bruno-scholz-b2a55723'>
+            <AiFillLinkedin color='orange' size={30} />
+          </a>
+        </div>
+      </div>
+      <div className='photo'>
+        <img src={image} className='profile-image' alt='me' />
+      </div>
+    </div>
+  )
+}
+
+export default Profile
