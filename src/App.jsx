9158ca45d8145964265b6321f638c3e4736abfb9
brunoscholz <brunoscholz@yahoo.de>
2024-04-05 08:16:33 -0300

    added vercel analytics and created env file for mode variable

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components'
 
 import Profile from './components/Profile'
-
 import Toggle from './components/Toggle/Toggle'
+
+import { Analytics } from '@vercel/analytics/react';
 
 import { useTheme } from './theme/useTheme';
 import { GlobalStyles, blockchainTheme, corporateTheme, gamingTheme, Header } from './theme/theme'
@@ -20,6 +21,7 @@ const App = () => {
     <ThemeProvider theme={themeMode}>
       <>
         <GlobalStyles />
+        <Analytics />
         <div style={{marginBottom: '2rem'}}>
           <Header>
             <Toggle theme={theme} onChange={switchTheme} />
