0134f916682852578ee0ff1aa974196995a9ce6e
brunoscholz <brunoscholz@yahoo.de>
2024-04-05 08:39:29 -0300

    issues with cached .env

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
+import React from 'react'
 import { ThemeProvider } from 'styled-components'
 
 import Profile from './components/Profile'
 import Toggle from './components/Toggle/Toggle'
 
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from '@vercel/analytics/react'
 
-import { useTheme } from './theme/useTheme';
+import { useTheme } from './theme/useTheme'
 import { GlobalStyles, blockchainTheme, corporateTheme, gamingTheme, Header } from './theme/theme'
 
 const App = () => {
-  const [ theme, switchTheme, componentMounted ] = useTheme()
-  const themeMode = theme === 'corporate' ? corporateTheme : theme === 'blockchain' ? blockchainTheme : gamingTheme;
+  const [theme, switchTheme, componentMounted] = useTheme()
+  const themeMode = theme === 'corporate' ? corporateTheme : theme === 'blockchain' ? blockchainTheme : gamingTheme
+  const mode = process.env.REACT_APP_MODE
+  console.log(mode)
 
   if (!componentMounted) {
     return <div />
@@ -21,8 +23,8 @@ const App = () => {
     <ThemeProvider theme={themeMode}>
       <>
         <GlobalStyles />
-        <Analytics />
-        <div style={{marginBottom: '2rem'}}>
+        <Analytics mode={mode} />
+        <div style={{ marginBottom: '2rem' }}>
           <Header>
             <Toggle theme={theme} onChange={switchTheme} />
           </Header>
